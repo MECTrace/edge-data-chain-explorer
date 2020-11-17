94c2c8579177483b16bdae7b6531e73559f96a4f
Chel Park <tradeoff@hanmail.net>
2020-11-17 17:14:07 +0900

    [ui] Fix API endpoints

--- react/src/ExplorerAPI.ts
+++ react/src/ExplorerAPI.ts
@@ -115,14 +115,14 @@ const fetchAccountIncentives = (
   chainId: string, address: string, top: number, from: number, num: number):
     Result<Incentive[]> => {
   return client
-    .get(`chain/${chainId}/accounts/${address}/incentives?top=${top}&from=${from}&num=${num}`)
+    .get(`chain/${chainId}/incentives/${address}?top=${top}&from=${from}&num=${num}`)
 }
 
 const fetchAccountPenalties = (
   chainId: string, address: string, top: number, from: number, num: number):
     Result<Penalty[]> => {
   return client
-    .get(`chain/${chainId}/accounts/${address}/penalties?from=${from}&num=${num}`)
+    .get(`chain/${chainId}/penalties/${address}?from=${from}&num=${num}`)
 }
 
 export default {
